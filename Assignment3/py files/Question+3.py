9042d23b30fab368d6637fc371c7b61a116ea76f
Dhruv Kanakia <kanakia.d@husky.neu.edu>
2017-04-06 23:26:24 -0400

    Final Push

--- Assignment3/py files/Question+3.py
+++ Assignment3/py files/Question+3.py
@@ -6,50 +6,55 @@
 
 
 
-# In[10]:
+# In[7]:
 
 import pandas as pd
 import numpy as np
 
 
-# In[1]:
+# In[8]:
 
 import os
 path=os.getcwd()
 
 
-# In[4]:
+# In[9]:
 
 final_path=path+ "\\Data\\cricket_matches.csv"
 
 
-# In[25]:
+# In[10]:
 
 df= pd.read_csv(final_path)
 
 
-# In[33]:
+# In[12]:
+
+df['team'] = np.where((df['home'] == df['winner']), df['home'], np.nan)
 
 
+# In[13]:
+
+df1=df[df.home==df.winner]
 
 
-# In[43]:
+# In[14]:
 
-df['winner_score'] = df[['innings1_runs','innings2_runs']].max(axis=1)
-
-
-# In[46]:
-
-df1=df.groupby('winner')['winner_score'].mean()
+df1['winner_score'] = df[['innings1_runs','innings2_runs']].max(axis=1)
 
 
-# In[47]:
+# In[17]:
+
+df1=df1.groupby('home')['winner_score'].mean()
+
+
+# In[18]:
 
 df2 = df1.to_frame().reset_index()
 df2['winner_score']= round(df2['winner_score'],2)
 
 
-# In[48]:
+# In[19]:
 
 df2.head()
 
